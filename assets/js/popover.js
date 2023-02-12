3e243d9784675e90859e2840c71a362f67764ecc
jethoof <102444466+jethoof@users.noreply.github.com>
2023-02-11 21:53:27 -0800

    revert to older version
    because the older version works

--- assets/js/popover.js
+++ assets/js/popover.js
@@ -1,70 +1,26 @@
 function htmlToElement(html) {
-  const template = document.createElement("template")
+  const template = document.createElement('template')
   html = html.trim()
   template.innerHTML = html
   return template.content.firstChild
 }
 
-function initPopover(baseURL, useContextualBacklinks, renderLatex) {
+function initPopover(baseURL) {
   const basePath = baseURL.replace(window.location.origin, "")
-  fetchData.then(({ content }) => {
-    const links = [...document.getElementsByClassName("internal-link")]
-    links
-      .filter(li => li.dataset.src || (li.dataset.idx && useContextualBacklinks))
-      .forEach(li => {
-        var el
-        if (li.dataset.ctx) {
-          const linkDest = content[li.dataset.src]
+  document.addEventListener("DOMContentLoaded", () => {
+    fetchData.then(({ content }) => {
+      const links = [...document.getElementsByClassName("internal-link")]
+      links.forEach(li => {
+        const linkDest = content[li.dataset.src.replace(basePath, "")]
+        if (linkDest) {
           const popoverElement = `<div class="popover">
     <h3>${linkDest.title}</h3>
-    <p>${highlight(removeMarkdown(linkDest.content), li.dataset.ctx)}...</p>
+    <p>${removeMarkdown(linkDest.content).split(" ", 20).join(" ")}...</p>
     <p class="meta">${new Date(linkDest.lastmodified).toLocaleDateString()}</p>
 </div>`
-          el = htmlToElement(popoverElement)
-        } else {
-          const linkDest = content[li.dataset.src.replace(/\/$/g, "").replace(basePath, "")]
-          if (linkDest) {
-            let splitLink = li.href.split("#")
-            let cleanedContent = removeMarkdown(linkDest.content)
-            if (splitLink.length > 1) {
-              let headingName = splitLink[1].replace(/\-/g, " ")
-              let headingIndex = cleanedContent.toLowerCase().indexOf("<b>" + headingName + "</b>")
-              cleanedContent = cleanedContent.substring(headingIndex, cleanedContent.length)
-            }
-            const popoverElement = `<div class="popover">
-    <h3>${linkDest.title}</h3>
-    <p>${cleanedContent.split(" ", 20).join(" ")}...</p>
-    <p class="meta">${new Date(linkDest.lastmodified).toLocaleDateString()}</p>
-</div>`
-            el = htmlToElement(popoverElement)
-          }
-        }
-
-        if (el) {
+          const el = htmlToElement(popoverElement)
           li.appendChild(el)
-          if (renderLatex) {
-            renderMathInElement(el, {
-              delimiters: [
-                { left: '$$', right: '$$', display: false },
-                { left: '$', right: '$', display: false },
-                { left: '\\(', right: '\\)', display: false },
-                { left: '\\[', right: '\\]', display: false }
-              ],
-              throwOnError: false
-            })
-          }
-
           li.addEventListener("mouseover", () => {
-            // fix tooltip positioning
-            window.FloatingUIDOM.computePosition(li, el, {
-              middleware: [window.FloatingUIDOM.offset(10), window.FloatingUIDOM.inline(), window.FloatingUIDOM.shift()],
-            }).then(({ x, y }) => {
-              Object.assign(el.style, {
-                left: `${x}px`,
-                top: `${y}px`,
-              })
-            })
-
             el.classList.add("visible")
           })
           li.addEventListener("mouseout", () => {
@@ -72,5 +28,6 @@ function initPopover(baseURL, useContextualBacklinks, renderLatex) {
           })
         }
       })
+    })
   })
 }
